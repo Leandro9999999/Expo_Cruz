c6ee1a048b95a376360a50494ba9c97a705990ea
Leandro9999999 <ledubiz99@gmail.com>
2026-09-12 11:11:12 -0400

    activar google analytics

--- app/data/productos.ts
+++ app/data/productos.ts
@@ -40,4 +40,16 @@ export const LISTA_PRODUCTOS: Producto[] = [
     manualPdf2: "/pdf/",
     videoYoutube: "https://www.youtube.com/watch?v=TU_LINK",
   },
+
+  {
+    id: "Vocom-II-Plus",
+    titulo: "Vocom II Plus",
+    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionLarga:
+      "Es una herramienta profesional diseñada para controlar módulos de hardware mediante Bluetooth (HC-05/06). Permite enviar valores de velocidad precisos con una interfaz visual intuitiva y fluida.",
+    imagenes: ["/img/vocom2.png"],
+    manualPdf1: "/pdf/PROBADOR VEB EPG.pdf",
+    manualPdf2: "/pdf/",
+    videoYoutube: "https://www.youtube.com/watch?v=TU_LINK",
+  },
 ];
