d63dbdb35e8414039f1eec200445f78a23716477
Leandro9999999 <ledubiz99@gmail.com>
2026-09-18 17:33:22 -0400

    actulisado 333

--- app/data/productos.ts
+++ app/data/productos.ts
@@ -14,7 +14,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "BT-CONTROLLER",
     titulo: "BT CONTROLLER",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "Es una herramienta profesional diseñada para controlar módulos de hardware mediante Bluetooth (HC-05/06). Permite enviar valores de velocidad precisos con una interfaz visual intuitiva y fluida.",
     imagenes: ["/img/bt3.png", "/img/bt2.png", "/img/bt4.png", "/img/bt1.png"],
@@ -25,7 +25,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "Truck Link",
     titulo: "Truck Link",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "TruckLink es un dispositivo de redque establece la comunicación entre los sistemas centrales y el equipo del cliente, gestionando el direccionamiento y latransferencia de paquetes de datos hacia la computadora delusuario.",
     descripcionLarga2:
@@ -37,7 +37,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "PROBADOR-VEB-EPG",
     titulo: "PROBADOR VEB EPG",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "Tester para diagnóstico de actuadores EPG, WasteGate y Válvula AVU en sistemas diésel (12V/24V), con pantalla digital para lecturas de voltaje/corriente en tiempo real, modo de prueba automático y control de frecuencia PWM.",
     imagenes: [
@@ -55,7 +55,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "FH-4 Expert",
     titulo: "FH-4 Expert",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "FH-4 Expert es una aplicación móvil desarrollada por DieselSoft, orientada a brindar soporte técnico a profesionales y técnicos de talleres especializados en diagnóstico y reparación de vehículos. La aplicación proporciona acceso a información técnica, parámetros de configuración y diagramas de referencia, permitiendo identificar y solucionar incidencias de manera más eficiente.",
     imagenes: ["/img/celular.png"],
@@ -64,7 +64,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "Ecu Repair-1.",
     titulo: "Ecu Repair-1.",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "Ecu Repair transforma imágenes técnicas en una experiencia interactiva. Permite localizar componentes, revisar conexiones, consultar esquemáticos y organizar información técnica en un solo lugar.",
     imagenes: ["/img/ecurepair.png"],
@@ -74,7 +74,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "DevTool-FH5",
     titulo: "DevTool-FH5",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "DevTool-FH5 es una aplicación de escritorio desarrollada por DieselSoft, diseñada específicamente para facilitar el trabajo de los técnicos. Su propósito es agilizar y simplificar la habilitación de parámetros, permitiendo realizar estos procesos de forma más rápida, sencilla y eficiente.",
     imagenes: ["/img/DevToolFH5.png", "/img/im65.png"],
@@ -84,7 +84,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "TechTool",
     titulo: "TechTool",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "TechTool es una plataforma integral de software de diagnóstico y mantenimiento para camiones ofrecida por Dieselsoft, diseñada para optimizar la eficiencia operativa de las flotas según la generación del vehículo.",
     descripcionLarga2:
@@ -101,7 +101,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "Vocom-II-Plus",
     titulo: "Vocom II Plus",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "La herramienta de diagnóstico de camiones Volvo Vocom 88894000 es la herramienta de diagnóstico OEM más reciente, genuina y única fabricada por Movimento en Suecia y diseñada para el mantenimiento de Volvo, MACK, Penta (motores marinos), UD, camiones Renault EURO 6, autobuses, máquinas de construcción y otros vehículos pesados que cubren todo el proceso de reparación mediante el uso de una computadora portátil de diagnóstico especial con software de diagnóstico configurado. Fue desarrollado para hacer las tareas del taller de reparaciones más fáciles y más eficientes.",
     imagenes: [
@@ -114,8 +114,8 @@ export const LISTA_PRODUCTOS: Producto[] = [
   },
   {
     id: "VOCOM-1",
-    titulo: "VOCOM-1",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    titulo: "VOCOM 1",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "La herramienta de diagnóstico de camiones Volvo Vocom 88890300 es la herramienta de diagnóstico OEM más reciente, genuina y única fabricada por Movimento en Suecia y diseñada para el mantenimiento de Volvo, MACK, Penta (motores marinos), UD, camiones Renault EURO 6, autobuses, máquinas de construcción y otros vehículos pesados que cubren todo el proceso de reparación mediante el uso de una computadora portátil de diagnóstico especial con software de diagnóstico configurado. Fue desarrollado para hacer las tareas del taller de reparaciones más fáciles y más eficientes.",
     imagenes: [
@@ -128,7 +128,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "VOLVO-IMPACT",
     titulo: "Software de diagnóstico VOLVO IMPACT",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "Volvo Impact es la plataforma integral para Windows (online/offline) que reúne todo lo necesario para el mantenimiento de camiones y autobuses Volvo (1975–2025): catálogo completo de piezas y repuestos estándar, manuales paso a paso con lista de herramientas, diagramas eléctricos y boletines técnicos por cada sistema, y el módulo TRD para el cálculo de tiempos estándar en la gestión de órdenes de trabajo.",
     imagenes: ["/img/software/volvo impact.png"],
@@ -137,7 +137,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "Getac",
     titulo: "Getac S410 DUAL BATTERY",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "Excelente durabilidad, gran autonomía de batería, 16 GB de RAM y pantalla Full HD. Ideal para diagnósticos automotrices, logística o trabajo técnico al aire libre.A considerar: El procesador (i5-6300U) es de 6ta generación (lanzado originalmente alrededor de 2015-2016), por lo que no cumple de forma oficial los requisitos de Windows 11. Es una máquina enfocada en resistencia física más que en potencia de cálculo moderna.",
 
@@ -152,7 +152,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "INTERFAZ SCANIA VCI3",
     titulo: "INTERFAZ SCANIA VCI3",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       "El Scania VCI3 es un adaptador de diagnóstico profesional diseñado para camiones, autobuses y vehículos pesados de la marca SCANIA fabricados a partir del año 2004. Su función principal es conectarse a la red CAN BUS del vehículo (con un rango de voltaje de 7 a 40V DC) y comunicarse directamente con el software de diagnóstico especializado Scania SDP3 (versión 2.42 o superior).Su característica más destacada es la conectividad inalámbrica por Wi-Fi (802.11b/g con cifrados WPA2, WPA y WEP), lo que permite a los técnicos trabajar sin depender de cables de datos que puedan dañarse en el taller. Para mayor flexibilidad, también incluye una opción de conexión física mediante un puerto USB 2.0.",
 
@@ -162,7 +162,7 @@ export const LISTA_PRODUCTOS: Producto[] = [
   {
     id: "Protector OBD II - 16 PINES",
     titulo: "Protector OBD II - 16 PINES",
-    descripcionCorta: "NUESTRA SUITE INSIGNIA PARA CONTROL OPERACIONAL.",
+    descripcionCorta: "Más información",
     descripcionLarga:
       " El Protector OBD II de 16 pines es un cable de extensión de 36 cm diseñado para proteger los conectores originales de interfaces de diagnóstico como VOCOM I, VOCOM II y otros equipos universales. Su función principal es evitar el desgaste, rotura o doblado de los pines de la herramienta principal durante el uso continuo.Gracias a su estructura flexible y moldeable fabricada en ABS y metal de alta resistencia, permite acceder fácilmente a tomas OBD2 ubicadas en posiciones incómodas dentro del vehículo. Mantiene el paso directo en sus 16 pines, garantizando compatibilidad total con todos los protocolos OBD2 estándar y resistiendo impactos y condiciones de taller.",
     imagenes: [
